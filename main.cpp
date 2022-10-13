fbf1a37d912fa61facd8fabef3d392413dc98912
CipollinoVP <yurich2002Vadim@gmail.com>
2022-10-13 21:00:38 +0300

    hhg

--- main.cpp
+++ main.cpp
@@ -2,12 +2,36 @@
 #include "vector"
 #include "cmath"
 #include "fstream"
+#include "sstream"
+#include "string"
 
 void w_out(std::vector<std::vector<double>> const& M, std::ofstream& out){
     for (int i = 0; i < M.size(); ++i) {
-        for (double j : M[i]) {
-            out << std::fixed << j << "  ";
+        out << "{ ";
+        for (int j = 0; j < M.size(); j++) {
+            if (M[i][j] == 0) {
+                out << 0;
+            } else if (M[i][j] == 1){
+                out << 1;
+            } else {
+                std::stringstream s;
+                s << M[i][j];
+                std::string str = s.str();
+                if (str.find('e',0) != -1) {
+                    str.replace(str.find('e', 0), 1, "*10^");
+                    if (str.find('+',0) != -1){
+                        str.replace(str.find('+', 0), 1, "");
+                    }
+                    out << str;
+                } else {
+                    out << M[i][j];
+                }
+            }
+            if (j != M.size()-1){
+                out << ", ";
+            }
         }
+        out << " } ";
         if (i != M.size()-1){
             out << std::endl;
         }
@@ -16,7 +40,24 @@ void w_out(std::vector<std::vector<double>> const& M, std::ofstream& out){
 
 void v_out(std::vector<double> const& V, std::ofstream& out){
     for (int i = 0; i < V.size(); ++i) {
-        out << std::fixed << V[i];
+        if (V[i] == 0) {
+            out << 0 << "  ";
+        } else if (V[i] == 1){
+            out << 1 << "  ";
+        } else {
+            std::stringstream s;
+            s << V[i];
+            std::string str = s.str();
+            if (str.find('e',0) != -1) {
+                str.replace(str.find('e', 0), 1, "*10^");
+                if (str.find('+',0) != -1){
+                    str.replace(str.find('+', 0), 1, "");
+                }
+                out << str << "  ";
+            } else {
+                out << V[i] << "  ";
+            }
+        }
         if (i != V.size()-1){
             out << std::endl;
         }
@@ -29,8 +70,8 @@ int main() {
     double b = 0.2;
     double U = 0.00001;
     double E = 1.1*std::pow(10,10);
-    int n = 101;
-    int m = 101;
+    int n = 71;
+    int m = 71;
     double h = (b-a)/(n-1);
     double tau = 2*M_PI/(m-1);
     std::vector<double> B(n*m*2);
@@ -66,16 +107,16 @@ int main() {
         M[2*j+1][n*m+m+j-1] = -E*h/(2*h*a*tau-2*h*a*nu*nu*tau);
         M[2*j+1][n*m+m+j+1] = E*h/(2*h*a*tau-2*h*a*nu*nu*tau);
     }
-    M[0][m-1] = -E*a*tau/(2*h*a*tau-2*h*a*nu*nu*tau);
-    M[0][2*m-1] = E*h*nu*tau/(h*a*tau-h*a*nu*nu*tau);
-    M[0][3*m-1] = E*a*tau/(2*h*a*tau-2*h*a*nu*nu*tau);
-    M[0][n*m+m] = -E*h*nu/(2*h*a*tau-2*h*a*nu*nu*tau);
-    M[0][n*m+2*m+1] = E*h*nu/(2*h*a*tau-2*h*a*nu*nu*tau);
-    M[1][m-1] = -E*a*tau*nu/(2*h*a*tau-2*h*a*nu*nu*tau);
-    M[1][2*m-1] = E*h*tau/(h*a*tau-h*a*nu*nu*tau);
-    M[1][3*m-1] = E*a*nu*tau/(2*h*a*tau-2*h*a*nu*nu*tau);
-    M[1][n*m+m] = -E*h/(2*h*a*tau-2*h*a*nu*nu*tau);
-    M[1][n*m+2*m+1] = E*h/(2*h*a*tau-2*h*a*nu*nu*tau);
+    M[2*(m-1)][m-1] = -E*a*tau/(2*h*a*tau-2*h*a*nu*nu*tau);
+    M[2*(m-1)][2*m-1] = E*h*nu*tau/(h*a*tau-h*a*nu*nu*tau);
+    M[2*(m-1)][3*m-1] = E*a*tau/(2*h*a*tau-2*h*a*nu*nu*tau);
+    M[2*(m-1)][n*m+m] = -E*h*nu/(2*h*a*tau-2*h*a*nu*nu*tau);
+    M[2*(m-1)][n*m+2*m+1] = E*h*nu/(2*h*a*tau-2*h*a*nu*nu*tau);
+    M[2*(m-1)+1][m-1] = -E*a*tau*nu/(2*h*a*tau-2*h*a*nu*nu*tau);
+    M[2*(m-1)+1][2*m-1] = E*h*tau/(h*a*tau-h*a*nu*nu*tau);
+    M[2*(m-1)+1][3*m-1] = E*a*nu*tau/(2*h*a*tau-2*h*a*nu*nu*tau);
+    M[2*(m-1)+1][n*m+m] = -E*h/(2*h*a*tau-2*h*a*nu*nu*tau);
+    M[2*(m-1)+1][n*m+2*m+1] = E*h/(2*h*a*tau-2*h*a*nu*nu*tau);
     for (int i = 1; i < n-1; ++i) {
         for (int j = 1; j < m-1; ++j) {
             M[2*m*i+2*j][(i-1)*m+j-1] = 0;
@@ -235,16 +276,16 @@ int main() {
         M[2*m*(n-1)+2*j+1][n*m+(n-1)*m-m+j+1] = E*h/(2*h*b*tau-2*h*b*nu*nu*tau);
         jn = j;
     }
-    for (int j = jn+1; 2*M_PI*j/m < 8*M_PI/4; ++j) {
+    for (int j = jn+1; j<m; ++j) {
         M[2*m*(n-1)+2*j][(n-1)*m+j] = 1;
         B[2*m*(n-1)+2*j] = -U*std::cos(2*M_PI*j/m);
         M[2*m*(n-1)+2*j+1][n*m+(n-1)*m+j] = 1;
         B[2*m*(n-1)+2*j+1] = U*std::sin(2*M_PI*j/m);
         jn = j;
     }
-    std::ofstream matrix_out("/nethome/student/FS19/FS2-x1/MKR/matrix.txt");
+    std::ofstream matrix_out("/home/vadik/matrix.txt");
     w_out(M,matrix_out);
-    std::ofstream vector_out("/nethome/student/FS19/FS2-x1/MKR/vector.txt");
+    std::ofstream vector_out("/home/vadik/vector.txt");
     v_out(B,vector_out);
     return 0;
 }
